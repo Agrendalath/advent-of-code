e20a455adb381db0434dcf1676412779dd94aebe
Agrendalath <piotr@surowiec.it>
2023-07-13 22:58:06 +0200

    refactor: reorganize days to be in the lexicographical order

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,47 @@
 use std::fs;
 
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
 mod day11;
 mod day12;
 mod day13;
 mod day14;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 
 fn main() {
     println!("Day 1");
-    day1::main();
+    day01::main();
 
     println!("\nDay 2");
-    day2::main();
+    day02::main();
 
     println!("\nDay 3");
-    day3::main();
+    day03::main();
 
     println!("\nDay 4");
-    day4::main();
+    day04::main();
 
     println!("\nDay 5");
-    day5::main();
+    day05::main();
 
     println!("\nDay 6");
-    day6::main();
+    day06::main();
 
     println!("\nDay 7");
-    day7::main();
+    day07::main();
 
     println!("\nDay 8");
-    day8::main();
+    day08::main();
 
     println!("\nDay 9");
-    day9::main();
+    day09::main();
 
     println!("\nDay 10");
     day10::main();
@@ -64,6 +64,6 @@ pub fn get_input(day: u8, test: bool) -> String {
     if test {
         input_file = "test_input";
     }
-    let path = format!("src/day{day}/{input_file}");
+    let path = format!("src/day{day:0>2}/{input_file}");
     fs::read_to_string(path).expect("Should have been able to read the file.")
 }
